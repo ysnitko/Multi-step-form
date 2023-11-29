f85cb43e2034ae53cfe61444b52f822482bf5525
yuri_sn <yuri.snitko@gmail.com>
2023-11-29 12:54:05 +0300

    add cost

--- src/constants.js
+++ src/constants.js
@@ -1,27 +1,33 @@
 export const steps = [
-  { step: 1, chapter: "YOUR INFO", stepHeader: "STEP 1" },
-  { step: 2, chapter: "SELECT PLAN", stepHeader: "STEP 2" },
-  { step: 3, chapter: "ADD-ONS", stepHeader: "STEP 3" },
-  { step: 4, chapter: "SUMMARY", stepHeader: "STEP 4" },
+  { step: 1, chapter: 'YOUR INFO', stepHeader: 'STEP 1' },
+  { step: 2, chapter: 'SELECT PLAN', stepHeader: 'STEP 2' },
+  { step: 3, chapter: 'ADD-ONS', stepHeader: 'STEP 3' },
+  { step: 4, chapter: 'SUMMARY', stepHeader: 'STEP 4' },
 ];
 
 export const addons = [
   {
-    id: "1",
-    title: "Online service",
-    cost: "",
-    requirements: "Access to multiplayer games",
+    id: '1',
+    title: 'Online service',
+    cost: '',
+    requirements: 'Access to multiplayer games',
   },
   {
-    id: "2",
-    title: "Larger storage",
-    cost: "",
-    requirements: "Extra 1TB of cloud save",
+    id: '2',
+    title: 'Larger storage',
+    cost: '',
+    requirements: 'Extra 1TB of cloud save',
   },
   {
-    id: "3",
-    title: "Custimixable Profile",
-    cost: "",
-    requirements: "Custom theme on oyur profile",
+    id: '3',
+    title: 'Custimixable Profile',
+    cost: '',
+    requirements: 'Custom theme on oyur profile',
+  },
+  {
+    id: '4',
+    title: 'examples',
+    cost: '',
+    requirements: 'Custom theme on oyur profile',
   },
 ];
